41c5383337c3d96dd6f5e2cbb01bf5de4adbee8a
Seth Curry <seth@scurry.io>
2023-10-18 16:34:28 -0500

    Cleaning up a commit

--- internal/steamgr/application_config.go
+++ internal/steamgr/application_config.go
@@ -92,20 +92,3 @@ func BuildSystemdUnitFile(conf *ApplicationConfig) (string, error) {
 
 	return builder.String(), nil
 }
-
-/*
-[Unit]
-Description=XMage Server
-After=networking.service
-
-[Service]
-Type=simple
-ExecStart=/opt/jdk/bin/java -Xms1024m -Xmx9000m -Dfile.encoding=UTF-8 -Djava.security.policy=./config/security.policy -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-1.4.50.jar
-Restart=always
-User=xmage
-Group=xmage
-WorkingDirectory=/usr/share/xmage/mage-server
-
-[Install]
-WantedBy=default.target
-*/
